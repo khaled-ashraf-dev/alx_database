35d590e767a6aba18e7a204c4dc5d19c0d518560
Your Name <you@example.com>
2023-08-17 16:00:01 +0300

    task 4

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
 -- Write a script that creates a table called first_table in the current database in your MySQL server.
-CREATE IF NOT EXISTS TABLE first_table (
+CREATE TABLE IF NOT EXISTS first_table (
     id INT, name VARCHAR(256)
 );
